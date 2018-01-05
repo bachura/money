239cbce0c50d4fd5570b1ec2d07470278cff5410
Bachura <bachura95@gmail.com>
2018-01-05 13:27:39 +0300

    production v4 popup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
+	// Pop up
+	$('#pop-up, #pop-up-two').click(function() {
+		$('#modal-window').addClass('active');
+		event.preventDefault()
+	});
+	$('.modal-close').click(function() {
+		$(this).parent().removeClass('active');
+	});
 
 }); 
--- send.php
+++ send.php
@@ -5,23 +5,26 @@ $name = htmlspecialchars($name);
 $name = urldecode($name);
 $name = trim($name);
 
+$sername = $_POST['sername'];
+$sername = htmlspecialchars($sername);
+$sername = urldecode($sername);
+$sername = trim($sername);
+
 $email = $_POST['email'];
 $email = htmlspecialchars($email);
 $email = urldecode($email);
 $email = trim($email);
 
-$message = $_POST['message'];
-$message = htmlspecialchars($message);
-$message = urldecode($message);
-$message = trim($message);
+$phone = $_POST['phone'];
+$phone = htmlspecialchars($phone);
+$phone = urldecode($phone);
+$phone = trim($phone);
 
 
-if (mail("new.lead2017new@gmail.com", "Вернем деньги", "Имя: ".$name. "\nE-mail: ".$email. "\nСообщение: " .$message , "From: new.lead2017new@gmail.com \r\n"))
-	{	echo "Сообщение отправлено, все хорошо!";
-		header("Location: ./index.html");
+if (mail("bachura95@gmail.com", "Вернем деньги", "Имя: ".$name. "\nФамилия: ".$sername. "\nE-mail: ".$email. "\nТелефон: ".$phone , "From: bachura95@gmail.com \r\n"))
+	{	
+		header("Location: http://glopart.ru/buy/182311");
 	} else { 
 		echo "при отправке сообщения возникли ошибки";
 	}
-
-
 ?>
